7760fd84e584d3ac1cbd7608b698260445c017c5
Ian Candy <iancandysemail@gmail.com>
2015-05-15 14:08:03 -0400

    add login feature - mvp solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,12 @@ class ApplicationController < Sinatra::Base
 		erb :"index"
 	end
 
+	post '/login' do
+		@user = User.find_by(name: params[:username])
+		session[:user] = @user
+		redirect to "/users/#{@user.id}"
+	end
+
 	get '/users/:id' do
 		@user = User.find(params[:id])
 		erb :"users/show"
@@ -20,5 +26,18 @@ class ApplicationController < Sinatra::Base
 	get '/photos' do
 		@photos = Photo.all
 		erb :"photos/index"
+	end
+
+	get '/photos/new' do
+		@photos = Photo.all
+		erb :"photos/new"
+	end
+
+	post '/addphoto' do
+		@photo = Photo.find(params[:photo_id])
+		@user = session[:user]
+		@photo.update(user: session[:user])
+		binding.pry
+		redirect to "/users/#{@user.id}"
 	end
 end
